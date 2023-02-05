13639b0159ceab11ed6e7353961676ddd6ef8984
dmdeugarte <39715792+dmdeugarte@users.noreply.github.com>
2023-02-04 19:13:42 -0600

    Delete Map.java

--- src/Map.java
+++
@@ -1,6 +0,0 @@
-// A Map is an array of Tiles, with an image representing the board
-// This image is created through the many Tiles, each holding their own image of the board
-public class Map
-{
-
-}
